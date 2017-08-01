a8e99674723a518c94515c7c3f6d6a24fd704025
KonH <konh@yandex.ru>
2017-08-01 20:01:07 +0700

    Initial Commit

---
+++ ToDoManager/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ToDoManager
+//
+//  Created by KonH on 01.08.17.
+//  Copyright © 2017 KonH. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
